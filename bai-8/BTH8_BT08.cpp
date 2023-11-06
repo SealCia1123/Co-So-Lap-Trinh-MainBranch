af934e2dddf33dac70b322afad44a960e153cc47
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-06 17:41:54 +0700

    Update BTH8_BT08.cpp

--- bai-8/BTH8_BT08.cpp
+++ bai-8/BTH8_BT08.cpp
@@ -2,8 +2,33 @@
 
 using namespace std;
 
+int countAppearTimes(int arr[], int size, int x)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (x == arr[i])
+            count++;
+    }
+    return count;
+}
+
 int main()
 {
-    cout << "Hello world";
+    int n;
+    cout << "Nhap vao so luong phan tu cua mang: ";
+    cin >> n;
+    int arr[n], x;
+    cout << "Nhap vao gia tri cua mang: ";
+    for (int &i: arr)
+        cin >> i;
+    cout << "Nhap vao gia tri can kiem tra: ";
+    cin >> x;
+
+    if (countAppearTimes(arr, n, x))
+        cout << "So lan xuat hien cua phan tu " << x << " la: " << countAppearTimes(arr, n, x) << endl;
+    else
+        cout << "Phan tu " << x << " khong xuat hien trong mang" << endl;
+
     return 0;
 }
