cee8db237b6bd4b15ccc9898da0920472252d753
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-09 10:43:31 +0700

    Update BTH7_BT10.cpp

--- bai-7/BTH7_BT10.cpp
+++ bai-7/BTH7_BT10.cpp
@@ -15,10 +15,15 @@ void sortTwoNumberDescend(int &a, int &b)
         swap(a, b);
 }
 
+void sortThreeNumberAscend(int &a, int &b, int &c)
+{
+
+}
+
 int main()
 {
     int a = 2, b = 1;
-    sortTwoNumberAscend(a, b);
+    sortTwoNumberDescend(a, b);
     cout << a << " " << b;
     return 0;
 }
