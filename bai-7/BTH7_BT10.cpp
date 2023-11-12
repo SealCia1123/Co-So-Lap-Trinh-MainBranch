2d1a606c341e18302501997de0dd2a22313df9be
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-12 22:40:36 +0700

    Update BTH7_BT10.cpp

--- bai-7/BTH7_BT10.cpp
+++ bai-7/BTH7_BT10.cpp
@@ -2,38 +2,14 @@
 
 using namespace std;
 
-void swap(int &a, int &b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
-}
+void swap(int &a, int &b);
 
-void twoNumDescend(int &a, int &b)
-{
-    if (a < b)
-        swap(a, b);
-}
+void twoNumDescend(int &a, int &b);
 
-void threeNumAscend(int &a, int &b, int &c)
-{
-    if (a > b)
-        swap(a, b);
-    if (a > c)
-        swap(a, c);
-    if (b > c)
-        swap(b, c);
-}
+void threeNumAscend(int &a, int &b, int &c);
 
-void threeNumDescend(int &a, int &b, int &c)
-{
-    if (a < b)
-        swap(a, b);
-    if (a < c)
-        swap(a, c);
-    if (b < c)
-        swap(b, c);
-}
+void threeNumDescend(int &a, int &b, int &c);
+
 
 int main()
 {
@@ -55,4 +31,37 @@ int main()
     threeNumDescend(num6, num7, num8);
     cout << "Thu tu 3 so giam dan: " << num6 << " " << num7 << " " << num8 << endl;
     return 0;
+}
+
+void threeNumDescend(int &a, int &b, int &c)
+{
+    if (a < b)
+        swap(a, b);
+    if (a < c)
+        swap(a, c);
+    if (b < c)
+        swap(b, c);
+}
+
+void threeNumAscend(int &a, int &b, int &c)
+{
+    if (a > b)
+        swap(a, b);
+    if (a > c)
+        swap(a, c);
+    if (b > c)
+        swap(b, c);
+}
+
+void twoNumDescend(int &a, int &b)
+{
+    if (a < b)
+        swap(a, b);
+}
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
 }
