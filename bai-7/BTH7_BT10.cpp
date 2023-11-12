e30ce6b83533c7a2070e9f5781d3b17cd651bfec
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-12 21:25:34 +0700

    Update BTH7_BT10.cpp

--- bai-7/BTH7_BT10.cpp
+++ bai-7/BTH7_BT10.cpp
@@ -15,17 +15,44 @@ void twoNumDescend(int &a, int &b)
         swap(a, b);
 }
 
-void sortThreeNumberAscend(int &a, int &b, int &c)
+void threeNumAscend(int &a, int &b, int &c)
 {
+    if (a > b)
+        swap(a, b);
+    if (a > c)
+        swap(a, c);
+    if (b > c)
+        swap(b, c);
+}
 
+void threeNumDescend(int &a, int &b, int &c)
+{
+    if (a < b)
+        swap(a, b);
+    if (a < c)
+        swap(a, c);
+    if (b < c)
+        swap(b, c);
 }
 
 int main()
 {
-    int a, b;
-    cout << "Nhap vao 2 so nguyen a, b: ";
-    cin >> a >> b;
-    twoNumDescend(a, b);
-    cout << "Thu tu 2 so giam dan: " << a << " " << b;
+    int num1, num2;
+    cout << "Nhap vao 2 so nguyen: ";
+    cin >> num1 >> num2;
+    twoNumDescend(num1, num2);
+    cout << "Thu tu 2 so giam dan: " << num1 << " " << num2 << endl;
+
+    int num3, num4, num5;
+    cout << "Nhap vao 3 so nguyen: ";
+    cin >> num3 >> num4 >> num5;
+    threeNumAscend(num3, num4, num5);
+    cout << "Thu tu 3 so tang dan: " << num3 << " " << num4 << " " << num5 << endl;
+
+    int num6, num7, num8;
+    cout << "Nhap vao 3 so nguyen: ";
+    cin >> num6 >> num7 >> num8;
+    threeNumAscend(num6, num7, num8);
+    cout << "Thu tu 3 so giam dan: " << num6 << " " << num7 << " " << num8 << endl;
     return 0;
 }
