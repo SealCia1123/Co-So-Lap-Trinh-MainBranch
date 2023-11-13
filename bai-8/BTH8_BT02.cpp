f265c5ccce2597aa8c93ef99dbb0bdddabb8dbfd
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-13 19:01:02 +0700

    Update BTH8_BT02.cpp

--- bai-8/BTH8_BT02.cpp
+++ bai-8/BTH8_BT02.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-// Định nghĩa hằng MAXSIZE = 100
+
 #define MAXSIZE 100
 
 using namespace std;
@@ -13,22 +13,16 @@ int main()
     cout << "Nhap vao gia tri cua mang: ";
     for (int i = 0; i < currentSize; i++)
     {
-        // Tạo biến temp tạm thời, nhập giá trị vào temp rồi đặt values[i] = temp
         int temp;
         cin >> temp;
         values[i] = temp;
     }
     for (int i = 0; i < currentSize; i++)
     {
-        // Kiểm tra nếu i là phần tử cuối sẽ không in ra dấu ',', còn lại in ra bình thường
         if (i == currentSize - 1)
-        {
             cout << values[i] << endl;
-        }
         else
-        {
             cout << values[i] << ", ";
-        }
     }
     return 0;
 }
