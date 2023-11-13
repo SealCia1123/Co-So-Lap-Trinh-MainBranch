aa5fb2e1a832e8c27549975cdd000bfe4943481d
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-13 18:04:02 +0700

    Update BTH8_BT11.cpp

--- bai-8/BTH8_BT11.cpp
+++ bai-8/BTH8_BT11.cpp
@@ -1,9 +1,41 @@
+// Sắp xếp phần tử trong mảng tăng dần
 #include <iostream>
 
 using namespace std;
 
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void sortArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+
 int main()
 {
+    int n;
+    cout << "Nhap vao so luong phan tu cua mang: ";
+    cin >> n;
+    int arr[n];
 
+    cout << "Nhap vao gia tri cua mang: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    sortArray(arr, n);
+    cout << "Thu tu mang sap xep tang dan: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     return 0;
 }
