1c1da83c3ad2f98725d509da450f7d167f076820
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-13 18:30:22 +0700

    Update BTH8_BT12.cpp

--- bai-8/BTH8_BT12.cpp
+++ bai-8/BTH8_BT12.cpp
@@ -2,8 +2,34 @@
 
 using namespace std;
 
+void deleteElement(int *arr, int x, int &n)
+{
+    for (int i = x; i < n; i++)
+        arr[i] = arr[i + 1];
+    n--;
+}
+
+void deleteDuplicate(int *&arr, int &n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+                deleteElement(arr, j, n);
+        }
+    }
+}
+
 int main()
 {
-
+    int n;
+    cout << "Nhap vao so luong phan tu cua mang: ";
+    cin >> n;
+    int arr[n];
+    cout << "Nhap vao gia tri cua mang: ";
+    for (int &i: arr)
+        cin >> i;
+    deleteElement(arr, n);
     return 0;
 }
