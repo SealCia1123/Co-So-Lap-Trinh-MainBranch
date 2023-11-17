7b0e825844233719fd7a7ef34e598bb403715d4f
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-17 12:51:46 +0700

    update BTH8-BT12

--- bai-8/BTH8_BT12.cpp
+++ bai-8/BTH8_BT12.cpp
@@ -1,22 +1,25 @@
+// viết hàm xóa các phần tử trùng nhau trong mảng
 #include <iostream>
 
 using namespace std;
 
-void deleteElement(int *arr, int x, int &n)
+void deleteElementArr(int arr[], int &n, int x)
 {
     for (int i = x; i < n; i++)
+    {
         arr[i] = arr[i + 1];
+    }
     n--;
 }
 
-void deleteDuplicate(int *&arr, int &n)
+void deleteDuplicate(int arr[], int &n)
 {
     for (int i = 0; i < n; i++)
     {
         for (int j = i + 1; j < n; j++)
         {
             if (arr[i] == arr[j])
-                deleteElement(arr, j, n);
+                deleteElementArr(arr, n, j);
         }
     }
 }
@@ -27,8 +30,12 @@ int main()
     cout << "Nhap vao so luong phan tu cua mang: ";
     cin >> n;
     int arr[n];
-    cout << "Nhap vao gia tri cua mang: ";
-    for (int &i: arr)
-        cin >> i;
+    cout << "Nhap vao cac phan tu cua mang: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    deleteDuplicate(arr, n);
+    for (int i: arr)
+        cout << i << " ";
     return 0;
 }
