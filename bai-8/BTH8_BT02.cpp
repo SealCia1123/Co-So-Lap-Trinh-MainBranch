4e46e1a3443b5da986abb7e701d9e5f909492eb9
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-17 12:25:16 +0700

    test from windows

--- bai-8/BTH8_BT02.cpp
+++ bai-8/BTH8_BT02.cpp
@@ -11,12 +11,14 @@ int main()
     cout << "Nhap vao so luong phan tu trong mang: ";
     cin >> currentSize;
     cout << "Nhap vao gia tri cua mang: ";
+
     for (int i = 0; i < currentSize; i++)
     {
         int temp;
         cin >> temp;
         values[i] = temp;
     }
+   
     for (int i = 0; i < currentSize; i++)
     {
         if (i == currentSize - 1)
