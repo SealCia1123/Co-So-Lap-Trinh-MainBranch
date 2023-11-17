fa16e644bb12eb69bb2589c5ad7a1a5e896826de
SealCia1123 <maoduchai404@gmail.com>
2023-11-17 19:13:53 +0700

    test

--- bai-8/BTH8_BT12.cpp
+++ bai-8/BTH8_BT12.cpp
@@ -3,39 +3,8 @@
 
 using namespace std;
 
-void deleteElementArr(int arr[], int &n, int x)
-{
-    for (int i = x; i < n; i++)
-    {
-        arr[i] = arr[i + 1];
-    }
-    n--;
-}
-
-void deleteDuplicate(int arr[], int &n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[i] == arr[j])
-                deleteElementArr(arr, n, j);
-        }
-    }
-}
 
 int main()
 {
-    int n;
-    cout << "Nhap vao so luong phan tu cua mang: ";
-    cin >> n;
-    int arr[n];
-    cout << "Nhap vao cac phan tu cua mang: ";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    deleteDuplicate(arr, n);
-    for (int i: arr)
-        cout << i << " ";
     return 0;
 }
