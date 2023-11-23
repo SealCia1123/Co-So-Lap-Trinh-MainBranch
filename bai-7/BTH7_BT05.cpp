e2aeb3da5fbc661bc0738fb7adc35ab31312744d
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2023-11-23 09:20:33 +0700

    Update BTH7_BT05.cpp

--- bai-7/BTH7_BT05.cpp
+++ bai-7/BTH7_BT05.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+void swap(int &a, int &b);
+
+void ascend(int &a, int &b);
+
+int main()
+{
+    int a, b;
+    cout << "Nhap vao 2 so nguyen a va b: ";
+    cin >> a >> b;
+    ascend(a, b);
+    cout << "Thu tu tang dan: " << a << ", " << b << endl;
+    return 0;
+}
+
 void swap(int &a, int &b)
 {
     int temp = a;
@@ -13,14 +27,4 @@ void ascend(int &a, int &b)
 {
     if (a >= b)
         swap(a, b);
-}
-
-int main()
-{
-    int a, b;
-    cout << "Nhap vao 2 so nguyen a va b: ";
-    cin >> a >> b;
-    ascend(a, b);
-    cout << "Thu tu tang dan: " << a << ", " << b << endl;
-    return 0;
 }
