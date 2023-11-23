e5441f97be261e4ac9ec3921dbeaf6008b48d0c6
SealCia1123 <maoduchai404@gmail.com>
2023-11-23 15:06:38 +0700

    update feature

--- bai-4/BTH4_BT13.cpp
+++ bai-4/BTH4_BT13.cpp
@@ -5,16 +5,23 @@ using namespace std;
 
 int main()
 {
-    int max = -1e8;
-    while (!cin.eof())
+    int max = -1e8, min = 1e8, sum = 0;
+    while (true)
     {
         int n;
         cout << "Nhap vao so nguyen n: ";
         cin >> n;
+        if (cin.get() == EOF)
+            break;
         if (n > max)
             max = n;
+        if (n < min)
+            min = n;
+        sum += n;
     }
     cout << "Gia tri lon nhat: " << max << endl;
+    cout << "Gia tri nho nhat: " << min << endl;
+    cout << "Tong cac so vua nhap la: " << sum << endl;
     system("pause");
     return 0;
 }
