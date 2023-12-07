4b876fd9acf8744461bfceddfe751c46ea9a9e69
SealCia1123 <maoduchai404@gmail.com>
2023-12-07 20:53:15 +0700

    Update format BTH5-BT03

--- bai-5/BTH5_BT03.cpp
+++ bai-5/BTH5_BT03.cpp
@@ -11,19 +11,42 @@ using namespace std;
 
 int main()
 {
-    int n;
-    double s1 = 0, s2 = 0, s3 = 0;
-    cout << "Nhap vao so nguyen duong n: ";
-    cin >> n;
-    for (int i = 1; i <= n; i++)
+    int choice, n;
+    double s;
+    cout << "1. S1 = 1 + 2^2 + 3^2 + ... + n^2\n";
+    cout << "2. S3 = 1 + 1/2 + 1/3 + ... + 1/n\n";
+    cout << "3. S3 = 1 + 1/3 + 1/5 + ... + 1/(2n-1)\n";
+
+    cout << "Nhap vao lua chon: ";
+    cin >> choice;
+    switch (choice)
     {
-        s1 += (double) i * i;
-        s2 += 1.0 / (double) i;
-        if (i % 2 != 0)
-            s3 += 1.0 / (double) i;
+        case 1:
+            cout << "Nhap vao so nguyen duong n: ";
+            cin >> n;
+            for (int i = 1; i <= n; i++)
+                s += i * i;
+            cout << "S1 = " << fixed << setprecision(0) << s << "\n";
+            break;
+        case 2:
+            cout << "Nhap vao so nguyen duong n: ";
+            cin >> n;
+            for (int i = 1; i <= n; i++)
+                s += (double) 1 / i;
+            cout << "S2 = " << fixed << setprecision(3) << s << "\n";
+            break;
+        case 3:
+            cout << "Nhap vao so nguyen duong n: ";
+            cin >> n;
+            for (int i = 1; i <= n; i += 2)
+                s += (double) 1 / i;
+            cout << "S3 = " << fixed << setprecision(3) << s << "\n";
+            break;
+        case 0:
+            cout << "Thoat chuong trinh\n";
+            return 0;
+        default:
+            cout << "Lua chon khong hop le\n";
     }
-    cout << "S1 = " << fixed << setprecision(0) << s1 << endl;
-    cout << "S2 = " << fixed << setprecision(3) << s2 << endl;
-    cout << "S3 = " << fixed << setprecision(3) << s3 << endl;
     return 0;
 }
