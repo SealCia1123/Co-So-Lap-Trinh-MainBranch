5318f7c39600c107631231b04c4736fa84b144b9
SealCia1123 <maoduchai404@gmail.com>
2023-12-07 22:12:27 +0700

    fix BT12

--- bai-5/BTH5_BT12.cpp
+++ bai-5/BTH5_BT12.cpp
@@ -5,45 +5,66 @@ using namespace std;
 
 int main()
 {
+    char choice;
     int h;
+    cout << "Nhap vao lua chon (a, b, c, d, e): ";
+    cin >> choice;
     cout << "Nhap vao chieu cao cua tam giac: ";
     cin >> h;
-
-    // hình a.
-    cout << "*\n";
-    for (int i = 2; i <= h - 1; i++)
-        cout << "*" << setw(i) << "*\n";
-    for (int i = 1; i <= h; i++)
-        cout << "*";
-
-    cout << endl;
-
-    // hình b.
-    for (int i = 1; i <= h; i++)
+    switch (choice)
     {
-        cout << setw(h - i + 1);
-        for (int j = 1; j <= i; j++)
-            cout << "*";
-        cout << endl;
+        case 'a':
+            // hình a.
+            cout << "*\n";
+            for (int i = 2; i <= h - 1; i++)
+                cout << "*" << setw(i) << "*\n";
+            for (int i = 1; i <= h; i++)
+                cout << "*";
+            break;
+        case 'b':
+            // hình b.
+            for (int i = 1; i <= h; i++)
+            {
+                cout << setw(h - i + 1);
+                for (int j = 1; j <= i; j++)
+                    cout << "*";
+                cout << endl;
+            }
+            break;
+        case 'c':
+            // hình c.
+            cout << endl;
+            for (int i = h; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                    cout << "*";
+                cout << endl;
+            }
+            break;
+        case 'd':
+            // hinh d
+            cout << endl;
+            for (int i = h; i >= 1; i--)
+            {
+                cout << setw(h - i + 1);
+                for (int j = 1; j <= i; j++)
+                    cout << "*";
+                cout << endl;
+            }
+            break;
+        case 'e':
+            for (int i = 1; i <= h; i++)
+            {
+                cout << setw(h - i + 2);
+                for (int j = 1; j <= i; j++)
+                    cout << "* ";
+                cout << endl;
+            }
+            break;
+        default:
+            cout << "Lua chon khong hop le\n";
     }
 
-    // hình c.
-    cout << endl;
-    for (int i = h; i >= 1; i--)
-    {
-        for (int j = 1; j <= i; j++)
-            cout << "*";
-        cout << endl;
-    }
-
-    // hinh d
-    cout << endl;
-    for (int i = h; i >= 1; i--)
-    {
-        for (int j = 1; j <= i; j++)
-            cout << setw(i - 1) << "*";
-        cout << endl;
-    }
 
     return 0;
 }
