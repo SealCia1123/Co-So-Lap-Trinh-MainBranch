35d71458b0d6697d8ea4eb8fd45859da477e340d
SealCia1123 <maoduchai404@gmail.com>
2023-12-14 10:07:45 +0700

    update

--- bai-7/BTH7_BT02.cpp
+++ bai-7/BTH7_BT02.cpp
@@ -10,9 +10,16 @@ int main()
     int tuSo, mauSo;
     cout << "Nhap vao lan luot tu so va mau so: ";
     cin >> tuSo >> mauSo;
-
-    cout << "Ket qua rut gon phan so: " << tuSo / uocChungLonNhat(tuSo, mauSo) << "/"
-         << mauSo / uocChungLonNhat(tuSo, mauSo) << endl;
+    if (mauSo == 0)
+        cout << "Loi chia cho 0" << endl;
+    else if (tuSo == 0)
+        cout << 0 << endl;
+    else
+    {
+        int uoc = uocChungLonNhat(tuSo, mauSo);
+        cout << "Ket qua rut gon phan so: " << tuSo / uoc << "/"
+             << mauSo / uoc << endl;
+    }
     return 0;
 }
 
@@ -20,9 +27,13 @@ int uocChungLonNhat(int m, int n)
 {
     if (n == 0)
         return m;
-    else
+//    int r = m % n;
+//    return uocChungLonNhat(n, r);
+    while (n)
     {
         int r = m % n;
-        return uocChungLonNhat(n, r);
+        m = n;
+        n = r;
     }
+    return m;
 }
