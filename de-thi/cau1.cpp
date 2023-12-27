400681f9873c70736ea449d7dedde3faa27e0c46
SealCia1123 <maoduchai404@gmail.com>
2023-12-27 18:16:22 +0700

    add comments

--- de-thi/cau1.cpp
+++ de-thi/cau1.cpp
@@ -26,35 +26,27 @@ int main()
         switch (choice)
         {
             case 1:
-                cout
-                        << "TIM CHU SO NHO NHAT TRONG MOT SO NGUYEN\n";
+                cout << "TIM CHU SO NHO NHAT TRONG MOT SO NGUYEN\n";
                 do
                 {
-                    cout
-                            << "Nhap vao so nguyen n (n > 0): ";
+                    cout << "Nhap vao so nguyen n (n > 0): ";
                     cin >> n;
                     if (n <= 0)
-                        cout
-                                << "Nhap sai, nhap lai n > 0\n";
+                        cout << "Nhap sai, nhap lai n > 0\n";
                 } while (n <= 0);
-                cout << "Chu so nho nhat trong " << n
-                     << " la: " << ChuSoNhoNhat(n) << endl;
+                cout << "Chu so nho nhat trong " << n << " la: " << ChuSoNhoNhat(n) << endl;
                 break;
 
             case 2:
-                cout
-                        << "Tinh S(n) = 1/3 + 1/5 - 1/7 + 1/9 ... + 1/(2n + 1)\n";
+                cout << "Tinh S(n) = 1/3 + 1/5 - 1/7 + 1/9 ... + 1/(2n + 1)\n";
                 do
                 {
-                    cout
-                            << "Nhap vao so nguyen n (n > 0): ";
+                    cout << "Nhap vao so nguyen n (n > 0): ";
                     cin >> n;
                     if (n <= 0)
-                        cout
-                                << "Nhap sai, nhap lai n > 0\n";
+                        cout << "Nhap sai, nhap lai n > 0\n";
                 } while (n <= 0);
-                cout << "S(" << n << "): " << fixed
-                     << setprecision(4) << s(n) << endl;
+                cout << "S(" << n << "): " << fixed << setprecision(4) << s(n) << endl;
                 break;
 
             default:
@@ -69,18 +61,24 @@ int main()
             cout << "Ket thuc chuong trinh\n";
             return 0;
         }
+        // Neu dung windows thi thay bang lenh cls
         system("clear");
     }
 }
 
 double s(int n)
 {
+    // Bat dau tu S = 1/3
     double result = (double) 1 / 3;
+    // Chay tu i = 2
     for (int i = 2; i <= n; i++)
     {
-        if (i & 1)
+        if (i % 2 == 1)
+            // Neu i la so le thi lay kq hien tai - di 1/(2i+1)
             result -= (double) 1 / (2 * i + 1);
+
         else
+            // Neu i la so chan thi lay kq hien tai + di 1/(2i+1)
             result += (double) 1 / (2 * i + 1);
     }
     return result;
@@ -102,7 +100,6 @@ int ChuSoNhoNhat(int n)
 void menu()
 {
     cout << "1. Tim so nho nhat trong mot so nguyen\n";
-    cout
-            << "2. Tinh S(n) = 1/3 + 1/5 - 1/7 + 1/9 ... + 1/(2n + 1)\n";
+    cout << "2. Tinh S(n) = 1/3 + 1/5 - 1/7 + 1/9 ... + 1/(2n + 1)\n";
     cout << "Chon 1 hoac 2: ";
 }
